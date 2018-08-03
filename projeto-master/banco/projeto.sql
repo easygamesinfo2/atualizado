953deaf2234d410d8bf4f1b71ed0bbe757425940
easygamesinfo2 <36482889+easygamesinfo2@users.noreply.github.com>
2018-08-03 16:08:28 -0300

    Add files via upload

--- projeto-master/banco/projeto.sql
+++ projeto-master/banco/projeto.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: 19-Jul-2018 às 21:42
--- Versão do servidor: 10.1.31-MariaDB
--- PHP Version: 7.2.4
+-- Host: localhost
+-- Generation Time: 03-Ago-2018 às 15:41
+-- Versão do servidor: 5.7.21-0ubuntu0.16.04.1
+-- PHP Version: 7.0.22-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,21 +23,16 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `avaliacao`
+-- Estrutura da tabela `avalia`
 --
 
-CREATE TABLE `avaliacao` (
-  `cod_jogo` int(11) DEFAULT NULL,
-  `cod_usuario` int(11) DEFAULT NULL,
-  `data_avaliacao` date DEFAULT NULL,
-  `velocidade` int(11) DEFAULT NULL,
-  `atualizacoes` int(11) DEFAULT NULL,
-  `diversao` int(11) DEFAULT NULL,
-  `audio` int(11) DEFAULT NULL,
-  `cod_avaliacao` int(11) NOT NULL,
-  `jogabilidade` int(11) DEFAULT NULL,
-  `social` int(11) DEFAULT NULL,
-  `grafico` int(11) DEFAULT NULL
+CREATE TABLE `avalia` (
+  `cod_avalia` int(11) NOT NULL,
+  `nome` varchar(30) DEFAULT NULL,
+  `descricao` varchar(200) DEFAULT NULL,
+  `data_avalia` date DEFAULT NULL,
+  `like` int(11) DEFAULT NULL,
+  `deslike` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -72,18 +65,6 @@ CREATE TABLE `comentario` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `jogo`
---
-
-CREATE TABLE `jogo` (
-  `cod_jogo` int(11) NOT NULL,
-  `nome` varchar(30) DEFAULT NULL,
-  `descricao` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `noticia`
 --
 
@@ -101,8 +82,13 @@ CREATE TABLE `noticia` (
 --
 
 INSERT INTO `noticia` (`cod_noticia`, `data_noticia`, `descricao`, `titulo_da_noticia`, `status`, `qtd`) VALUES
-(65, '2018-07-10', 'b', 'a', 1, 9),
-(66, '2018-07-19', 'd', 'c', 1, 0);
+(65, '2018-07-10', 'b', 'a', 1, 24),
+(66, '2018-07-19', 'd', 'c', 1, 15),
+(67, '2018-08-03', 'adjakdas\r\n', 'asadas', 0, 0),
+(68, '2018-08-03', 'dfasda', 'daassdas', 1, 0),
+(69, '2018-08-03', 'fawawfwa', 'fafaw', 1, 0),
+(70, '2018-08-03', 'aaaa', 'aaa', 1, 0),
+(71, '2018-08-03', 'aaaaaaaaaaaaaaaa', 'jafklwfnhklqan', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -142,12 +128,10 @@ INSERT INTO `usuario` (`cod_usuario`, `senha`, `email`, `nome`, `cod_tip`) VALUE
 --
 
 --
--- Indexes for table `avaliacao`
+-- Indexes for table `avalia`
 --
-ALTER TABLE `avaliacao`
-  ADD PRIMARY KEY (`cod_avaliacao`),
-  ADD KEY `cod_jogo` (`cod_jogo`),
-  ADD KEY `cod_usuario` (`cod_usuario`);
+ALTER TABLE `avalia`
+  ADD PRIMARY KEY (`cod_avalia`);
 
 --
 -- Indexes for table `cadastrar`
@@ -165,12 +149,6 @@ ALTER TABLE `comentario`
   ADD KEY `cod_usuario` (`cod_usuario`),
   ADD KEY `cod_jogo` (`cod_jogo`),
   ADD KEY `cod_noticia` (`cod_noticia`);
-
---
--- Indexes for table `jogo`
---
-ALTER TABLE `jogo`
-  ADD PRIMARY KEY (`cod_jogo`);
 
 --
 -- Indexes for table `noticia`
@@ -196,58 +174,40 @@ ALTER TABLE `usuario`
 --
 
 --
--- AUTO_INCREMENT for table `avaliacao`
+-- AUTO_INCREMENT for table `avalia`
 --
-ALTER TABLE `avaliacao`
-  MODIFY `cod_avaliacao` int(11) NOT NULL AUTO_INCREMENT;
-
+ALTER TABLE `avalia`
+  MODIFY `cod_avalia` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `comentario`
 --
 ALTER TABLE `comentario`
   MODIFY `cod_comentario` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `jogo`
---
-ALTER TABLE `jogo`
-  MODIFY `cod_jogo` int(11) NOT NULL AUTO_INCREMENT;
-
 --
 -- AUTO_INCREMENT for table `noticia`
 --
 ALTER TABLE `noticia`
-  MODIFY `cod_noticia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
-
+  MODIFY `cod_noticia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
 --
 -- AUTO_INCREMENT for table `tip_user`
 --
 ALTER TABLE `tip_user`
   MODIFY `cod_tip` int(11) NOT NULL AUTO_INCREMENT;
-
 --
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
   MODIFY `cod_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
 --
 -- Constraints for dumped tables
 --
-
---
--- Limitadores para a tabela `avaliacao`
---
-ALTER TABLE `avaliacao`
-  ADD CONSTRAINT `avaliacao_ibfk_1` FOREIGN KEY (`cod_jogo`) REFERENCES `jogo` (`cod_jogo`),
-  ADD CONSTRAINT `avaliacao_ibfk_2` FOREIGN KEY (`cod_usuario`) REFERENCES `usuario` (`cod_usuario`);
 
 --
 -- Limitadores para a tabela `cadastrar`
 --
 ALTER TABLE `cadastrar`
   ADD CONSTRAINT `cadastrar_ibfk_1` FOREIGN KEY (`cod_usuario`) REFERENCES `usuario` (`cod_usuario`),
-  ADD CONSTRAINT `cadastrar_ibfk_2` FOREIGN KEY (`cod_jogo`) REFERENCES `jogo` (`cod_jogo`),
+  ADD CONSTRAINT `cadastrar_ibfk_2` FOREIGN KEY (`cod_jogo`) REFERENCES `avalia` (`cod_avalia`),
   ADD CONSTRAINT `cadastrar_ibfk_3` FOREIGN KEY (`cod_noticia`) REFERENCES `noticia` (`cod_noticia`);
 
 --
@@ -255,7 +215,7 @@ ALTER TABLE `cadastrar`
 --
 ALTER TABLE `comentario`
   ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`cod_usuario`) REFERENCES `usuario` (`cod_usuario`),
-  ADD CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`cod_jogo`) REFERENCES `jogo` (`cod_jogo`),
+  ADD CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`cod_jogo`) REFERENCES `avalia` (`cod_avalia`),
   ADD CONSTRAINT `comentario_ibfk_3` FOREIGN KEY (`cod_noticia`) REFERENCES `noticia` (`cod_noticia`);
 
 --
@@ -263,7 +223,6 @@ ALTER TABLE `comentario`
 --
 ALTER TABLE `usuario`
   ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`cod_tip`) REFERENCES `tip_user` (`cod_tip`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
