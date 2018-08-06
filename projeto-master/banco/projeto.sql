f5da54475acd61206d35b64573622d32ca169269
easygamesinfo2 <36482889+easygamesinfo2@users.noreply.github.com>
2018-08-05 22:38:43 -0300

    Add files via upload

--- projeto-master/banco/projeto.sql
+++ projeto-master/banco/projeto.sql
@@ -1,229 +1,38 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: 03-Ago-2018 Ã s 15:41
--- VersÃ£o do servidor: 5.7.21-0ubuntu0.16.04.1
--- PHP Version: 7.0.22-0ubuntu0.16.04.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+-- Geração de Modelo físico
+-- Sql ANSI 2003 - brModelo.
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `projeto`
---
+CREATE TABLE usuario (
+cod_usuario int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+senha varchar(15),
+email varchar(50),
+nome varchar(50),
+tipo_user int
+);
 
--- --------------------------------------------------------
+CREATE TABLE noticia (
+cod_noticia int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+data_noticia date,
+descricao text,
+status tinyint,
+titulo varchar(150),
+qtd tinyint
+);
 
---
--- Estrutura da tabela `avalia`
---
+CREATE TABLE avaliacao (
+cod_avaliacao int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nome varchar(100),
+descricao text,
+data_avaliacao date
+);
 
-CREATE TABLE `avalia` (
-  `cod_avalia` int(11) NOT NULL,
-  `nome` varchar(30) DEFAULT NULL,
-  `descricao` varchar(200) DEFAULT NULL,
-  `data_avalia` date DEFAULT NULL,
-  `like` int(11) DEFAULT NULL,
-  `deslike` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE cadastrar (
+cod_usuario int,
+cod_noticia int,
+cod_avaliacao int,
+FOREIGN KEY(cod_usuario) REFERENCES usuario (cod_usuario),
+FOREIGN KEY(cod_noticia) REFERENCES noticia (cod_noticia),
+FOREIGN KEY(cod_avaliacao) REFERENCES avaliacao (cod_avaliacao)
+);
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `cadastrar`
---
-
-CREATE TABLE `cadastrar` (
-  `cod_usuario` int(11) DEFAULT NULL,
-  `cod_jogo` int(11) DEFAULT NULL,
-  `cod_noticia` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `comentario`
---
-
-CREATE TABLE `comentario` (
-  `cod_usuario` int(11) DEFAULT NULL,
-  `cod_jogo` int(11) DEFAULT NULL,
-  `cod_noticia` int(11) DEFAULT NULL,
-  `texto_comentario` varchar(100) DEFAULT NULL,
-  `data_comentario` date DEFAULT NULL,
-  `cod_comentario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `noticia`
---
-
-CREATE TABLE `noticia` (
-  `cod_noticia` int(11) NOT NULL,
-  `data_noticia` date DEFAULT NULL,
-  `descricao` varchar(200) DEFAULT NULL,
-  `titulo_da_noticia` varchar(50) DEFAULT NULL,
-  `status` tinyint(1) DEFAULT NULL,
-  `qtd` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `noticia`
---
-
-INSERT INTO `noticia` (`cod_noticia`, `data_noticia`, `descricao`, `titulo_da_noticia`, `status`, `qtd`) VALUES
-(65, '2018-07-10', 'b', 'a', 1, 24),
-(66, '2018-07-19', 'd', 'c', 1, 15),
-(67, '2018-08-03', 'adjakdas\r\n', 'asadas', 0, 0),
-(68, '2018-08-03', 'dfasda', 'daassdas', 1, 0),
-(69, '2018-08-03', 'fawawfwa', 'fafaw', 1, 0),
-(70, '2018-08-03', 'aaaa', 'aaa', 1, 0),
-(71, '2018-08-03', 'aaaaaaaaaaaaaaaa', 'jafklwfnhklqan', 1, 0);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tip_user`
---
-
-CREATE TABLE `tip_user` (
-  `cod_tip` int(11) NOT NULL,
-  `desc_tip_user` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuario`
---
-
-CREATE TABLE `usuario` (
-  `cod_usuario` int(11) NOT NULL,
-  `senha` varchar(30) DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `nome` varchar(50) DEFAULT NULL,
-  `cod_tip` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `usuario`
---
-
-INSERT INTO `usuario` (`cod_usuario`, `senha`, `email`, `nome`, `cod_tip`) VALUES
-(1, '123', 'soka@gmail.com', 'Gabriel Sokacheski', NULL),
-(2, '123', 'dudu@gmail.com', 'Carlos Eduardo', NULL);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `avalia`
---
-ALTER TABLE `avalia`
-  ADD PRIMARY KEY (`cod_avalia`);
-
---
--- Indexes for table `cadastrar`
---
-ALTER TABLE `cadastrar`
-  ADD KEY `cod_usuario` (`cod_usuario`),
-  ADD KEY `cod_jogo` (`cod_jogo`),
-  ADD KEY `cod_noticia` (`cod_noticia`);
-
---
--- Indexes for table `comentario`
---
-ALTER TABLE `comentario`
-  ADD PRIMARY KEY (`cod_comentario`),
-  ADD KEY `cod_usuario` (`cod_usuario`),
-  ADD KEY `cod_jogo` (`cod_jogo`),
-  ADD KEY `cod_noticia` (`cod_noticia`);
-
---
--- Indexes for table `noticia`
---
-ALTER TABLE `noticia`
-  ADD PRIMARY KEY (`cod_noticia`);
-
---
--- Indexes for table `tip_user`
---
-ALTER TABLE `tip_user`
-  ADD PRIMARY KEY (`cod_tip`);
-
---
--- Indexes for table `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`cod_usuario`),
-  ADD KEY `cod_tip` (`cod_tip`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `avalia`
---
-ALTER TABLE `avalia`
-  MODIFY `cod_avalia` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `comentario`
---
-ALTER TABLE `comentario`
-  MODIFY `cod_comentario` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `noticia`
---
-ALTER TABLE `noticia`
-  MODIFY `cod_noticia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
---
--- AUTO_INCREMENT for table `tip_user`
---
-ALTER TABLE `tip_user`
-  MODIFY `cod_tip` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `cod_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- Constraints for dumped tables
---
-
---
--- Limitadores para a tabela `cadastrar`
---
-ALTER TABLE `cadastrar`
-  ADD CONSTRAINT `cadastrar_ibfk_1` FOREIGN KEY (`cod_usuario`) REFERENCES `usuario` (`cod_usuario`),
-  ADD CONSTRAINT `cadastrar_ibfk_2` FOREIGN KEY (`cod_jogo`) REFERENCES `avalia` (`cod_avalia`),
-  ADD CONSTRAINT `cadastrar_ibfk_3` FOREIGN KEY (`cod_noticia`) REFERENCES `noticia` (`cod_noticia`);
-
---
--- Limitadores para a tabela `comentario`
---
-ALTER TABLE `comentario`
-  ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`cod_usuario`) REFERENCES `usuario` (`cod_usuario`),
-  ADD CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`cod_jogo`) REFERENCES `avalia` (`cod_avalia`),
-  ADD CONSTRAINT `comentario_ibfk_3` FOREIGN KEY (`cod_noticia`) REFERENCES `noticia` (`cod_noticia`);
-
---
--- Limitadores para a tabela `usuario`
---
-ALTER TABLE `usuario`
-  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`cod_tip`) REFERENCES `tip_user` (`cod_tip`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
